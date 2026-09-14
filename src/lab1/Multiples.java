f95ae6fbe34872d83f3c523fdb9c30bb576acb32
Steven Wang <stevennwang521@gmail.com>
2026-09-14 14:57:58 -0400

    Overloaded multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Multiples {
     public static void main() {
-        System.out.println(multiples(16, 5, 3));
+        System.out.println(multiples(1000, 3, 5));
     }
 
     static int multiples(int n, int a, int b) {
@@ -12,6 +12,25 @@ public class Multiples {
         while (i < n) {
             boolean divisibleByA = i % a == 0;
             boolean divisibleByB = i % b == 0;
+
+            if (divisibleByA || divisibleByB) {
+                System.out.println(i);
+                mults++;
+            }
+
+            i++;
+        }
+
+        return(mults);
+    }
+
+    static int multiples() {
+        int mults = 0;
+        int i = 1;
+
+        while (i < 1000) {
+            boolean divisibleByA = i % 3 == 0;
+            boolean divisibleByB = i % 5 == 0;
 
             if (divisibleByA || divisibleByB) {
                 System.out.println(i);
