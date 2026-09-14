71c3c3543d3f56c7cd4e880821bd9ab328c23df8
Steven Wang <stevennwang521@gmail.com>
2026-09-14 14:20:18 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -32,5 +32,35 @@ class FizzBuzz {
 
             }
         }
+
+        int j = 0;
+
+        while (j <= 100) {
+            j = doFizzBuzz(j);
+        }
+
+    }
+
+    private static int doFizzBuzz(int j) {
+        boolean divisibleBy3 = j % 3 == 0;
+        boolean divisibleBy5 = j % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+
+        } else {
+            System.out.println(j);
+
+        }
+
+        j++;
+        return j;
     }
 }
